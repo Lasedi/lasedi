6b360b3a5dd582bbe61fc735fb77911e41df80d0
Fiona Komane <komanelasedi@gmail.com>
2024-04-15 19:10:03 +0200

    Added 7kyu challenges

---
+++ 7kyu.py
@@ -0,0 +1,82 @@
+DESCRIPTION:
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+isIsogram "Dermatoglyphics" = true
+isIsogram "moose" = false
+isIsogram "aba" = false
+
+# my solution
+def is_isogram(string):
+    return len(set(string.lower())) == len(string.lower())
+
+
+DESCRIPTION:
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation)))
+
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+BINARYFUNDAMENTALS
+
+# my solution
+def add_binary(a,b):
+    return bin(a + b)[2:]
+
+
+DESCRIPTION:
+You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+Examples:(Input --> Output)
+
+121 --> 144
+625 --> 676
+114 --> -1 since 114 is not a perfect square
+
+# my solution
+def find_next_square(sq):
+    root = int(sq ** 0.5)
+    if root * root == sq:
+        return (root + 1) ** 2
+    else:
+        return -1
+
+
+
+DESCRIPTION:
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+STRINGSFUNDAMENTALS
+
+# my solution
+def get_count(sentence):
+    vowels = 'aeiou'
+    return sum(1 for char in sentence if char in vowels)
+
+
+DESCRIPTION:
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') # returns true
+solution('abc', 'd') # returns false
+STRINGSFUNDAMENTALS
+
+# my solution
+def solution(text, ending):
+    return text.endswith(ending)
